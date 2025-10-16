4a1002b20a117e811de8492596434e92f3364ca7
Ignacio <ignayh2000@gmail.com>
2025-10-16 11:40:14 +0200

    refactor: Improve code.

--- src/components/pages/ResourcesComponents/ArmoryCard.tsx
+++ src/components/pages/ResourcesComponents/ArmoryCard.tsx
@@ -4,26 +4,26 @@ import ArmoryItemComponent from "./ArmoryItemComponent";
 
 
 
-function ArmoryCard({weapons, shields}: any){
+function ArmoryCard({ weapons, shields }: any) {
 
     const armoryItems = [weapons, shields];
     const armoryItemsTittle = ["Weapons", "Shields"];
 
     return (
         <>
-        {(armoryItems).map((armoryItems: ArmoryItem[], index: number) => {
-            return (
-                <>
-                    <h1>{armoryItemsTittle[index]}</h1>
-                    {
-                        armoryItems.map((armoryItem: ArmoryItem, index: number) => {
-                            return(<ArmoryItemComponent key={index} type={armoryItem.type} quantity={armoryItem.quantity}/>);
-                        })
-                    }
-                </>
-            );
+            {(armoryItems).map((armoryItems: ArmoryItem[], index: number) => {
+                return (
+                    <>
+                        <h1>{armoryItemsTittle[index]}</h1>
+                        {
+                            armoryItems.map((armoryItem: ArmoryItem, index: number) => {
+                                return (<ArmoryItemComponent key={index} type={armoryItem.type} quantity={armoryItem.quantity} />);
+                            })
+                        }
+                    </>
+                );
 
-        })}
+            })}
 
         </>
     );
