778038121fa3f59f487694168c137ba18b7d7dd3
Ignacio <ignayh2000@gmail.com>
2025-10-16 09:15:25 +0200

    feat: Create navigation menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import Navigator from './components/Navigator';
 
 function App() {
   const [citadelData, setCitadelData] = useState(null);
 
   // --- fetch data from ./public/citadel.json ---
   const getCitadelData = ( () => {
-    const response = fetch("citadel.json")
+    const response = fetch("citadel.json"); 
     response.then((data) => {data.json().then(
       (result) => {setCitadelData(result)}
     )});
@@ -15,9 +16,8 @@ function App() {
 
   return (
     <div>
-      <h1>Citadel of Eternal Night</h1>
-      <p>Construye aquí la interfaz y lógica de la ciudadela.</p>
-      <p>{JSON.stringify(citadelData)}</p>
+      {/* --- Generate Navigator tabs --- */}
+      <Navigator />
     </div>
   )
 }
--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -1,13 +1,39 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Location from "./pages/Location";
+import Defenses from "./pages/Defenses";
+import Inhabitants from "./pages/Inhabitants";
+import Resources from "./pages/Resources";
+import History from "./pages/History";
 
 
-function Navigator(){
+function Navigator() {
 
 
-    return(
+    return (
         <>
-        
+
+            <div>
+                <Link to="/">Location</Link> | {" "}
+                <Link to="/Defenses">Defenses</Link> | {" "}
+                <Link to="/Inhabitants">Inhabitants</Link> | {" "}
+                <Link to="/Resources">Resources</Link> | {" "}
+                <Link to="/History">History</Link> | {" "}
+            </div>
+            <div>
+                <Routes>
+                    <Route path="/" element={<Location />} />
+                    <Route path="/Defenses" element={<Defenses />} />
+                    <Route path="/Inhabitants" element={<Inhabitants />} />
+                    <Route path="/Resources" element={<Resources />} />
+                    <Route path="/History" element={<History />} />
+                </Routes>
+            </div>
+
+
         </>
     );
 }
 
 export default Navigator;
+
+
